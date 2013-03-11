056e07693e24e76acd8c1647e677e4abaedf6141
guillaume.giamarchi@gmail.com <guillaume.giamarchi@gmail.com>
2013-03-11 11:41:58 +0100

    Refactoring

--- i18n-generator-maven-plugin/src/main/java/com/github/ggiamarchi/i18n/maven/GeneratorPlugin.java
+++ i18n-generator-maven-plugin/src/main/java/com/github/ggiamarchi/i18n/maven/GeneratorPlugin.java
@@ -11,7 +11,7 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.slf4j.impl.StaticLoggerBinder;
 
-import com.github.ggiamarchi.i18n.Launcher;
+import com.github.ggiamarchi.i18n.GeneratorLauncher;
 
 @Mojo(name = "generate", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
 public class GeneratorPlugin extends AbstractMojo {
@@ -38,7 +38,7 @@ public class GeneratorPlugin extends AbstractMojo {
 	    	res[i] = resourcesDirectories.get(i).getDirectory();
 	    }
 	    
-	    new Launcher().run(bundles.split(","), srcDirectory, res, outputDirectory);
+	    new GeneratorLauncher().execute(bundles.split(","), srcDirectory, res, outputDirectory);
 
     }
 
